9633fdecb6e3f0d98d7165d700076fae808b6eae
Rafael Santos <rafael.santos.193@gmail.com>
2016-06-23 13:27:24 -0300

    Implementando SVM

--- src/rafael_processing.py
+++ src/rafael_processing.py
@@ -9,6 +9,7 @@ import numpy as np
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.feature_extraction import FeatureHasher
 from sklearn.preprocessing import LabelEncoder
+from sklearn import preprocessing
 from sklearn.naive_bayes import MultinomialNB
 from sklearn import cross_validation
 import sys
@@ -22,7 +23,8 @@ import sys
 class animal_shelter:
 
     def __init__(self):
-        pass
+        self.flag_save_files = True
+        self.flag_restore_session = True
 
     def parse_age(self, row):
         age = row
@@ -42,6 +44,30 @@ class animal_shelter:
             row = quantity
         return row
 
+    # N = number of unique values
+    def keep_most_popular_values_for_feature(self, feature_name, N=30):
+        total = self.df[feature_name].shape[0]
+        counts = self.df[feature_name].value_counts()
+        sum = 0
+        different_values = 0
+        outros_list = list()
+        for value, count in counts.iteritems():
+            outros = False
+            #print value, count
+            sum += count
+            different_values += 1
+            #if sum > 0.9*total:
+            #    outros = True
+            if different_values >= N:
+                outros = True
+            if outros:
+                outros_list.append(value)
+        #print '---------------------'
+        #print 'Total\tSum\t%\tUnique'
+        #print '%d\t%d\t%.1f\t%d' % (total, sum, 100.0*float(sum)/float(total), different_values)
+        #print '---------------------'
+        self.df[feature_name].replace(outros_list, 'Outros', inplace=True)
+
     def shuffle(self):
         self.df = shuffle.shuffle(self.df)
 
@@ -50,6 +76,10 @@ class animal_shelter:
 
     def convert_age_to_numerical(self):
         self.df['AgeuponOutcome'] = self.df['AgeuponOutcome'].apply(self.parse_age)
+        self.df['AgeuponOutcome'].fillna(self.df['AgeuponOutcome'].mean(), inplace=True)
+        #print self.df['AgeuponOutcome'].as_matrix().dtype
+        #print preprocessing.scale(self.df['AgeuponOutcome'].as_matrix())
+        self.df['AgeuponOutcome'] = preprocessing.scale(self.df['AgeuponOutcome'].as_matrix())
         self.save_to_file(self.df, 'convert_age_to_numerical_df.csv')
 
     def categorical_to_numerical_one_hot(self):
@@ -58,6 +88,7 @@ class animal_shelter:
         self.vect = vect
         input_dict = self.input_df.T.to_dict().values()
         self.input_df = vect.fit_transform(input_dict)
+        #print self.input_df
         print 'Done.'
         self.save_to_file(self.input_df, 'categorial_to_numerical_one_hot_input_df.csv')
 
@@ -68,12 +99,10 @@ class animal_shelter:
         self.save_to_file(self.input_df, 'categorial_to_numerical_unique.csv')
 
     def target_to_numerical(self):
-        output = self.dataset[:][self.output_variable]
-        le = LabelEncoder()
-        le.fit(output.values.flatten())
-        self.le = le
-        output = le.transform(output.values.flatten())
+        print 'Converting target to numerical...'
+        self.target_df = self.target_df.apply(LabelEncoder().fit_transform)
         print 'Done.'
+        self.save_to_file(self.target_df, 'target_to_numerical_target_df.csv')
 
     def load_train_dataset(self):
         self.df = pandas.read_csv('../data/train.csv.gz', compression='gzip')
@@ -90,26 +119,44 @@ class animal_shelter:
         self.train_end_index = te
         self.validation_start_index = vs
         self.validation_end_index = ve
-        self.dataset = self.df[self.train_start_index:self.train_end_index][:]
-        self.validation = self.df[self.validation_start_index:self.validation_end_index][:]
+        self.train_input = self.input_df[self.train_start_index:self.train_end_index][:]
+        self.save_to_file(self.train_input, 'split_dataset_train_input.csv')
+        self.train_target = self.target_df[self.train_start_index:self.train_end_index][:]
+        self.save_to_file(self.train_target, 'split_dataset_train_target.csv')
+        self.validation_input = self.input_df[self.validation_start_index:self.validation_end_index][:]
+        self.save_to_file(self.validation_input, 'split_dataset_validation_input.csv')
+        self.validation_target = self.target_df[self.validation_start_index:self.validation_end_index][:]
+        self.save_to_file(self.validation_target, 'split_dataset_validation_target.csv')
+
+    def restore_session(self):
+        self.train_input = self.file_read('split_dataset_train_input.csv')
+        self.train_target = self.file_read('split_dataset_train_target.csv')
+        self.validation_input = self.file_read('split_dataset_validation_input.csv')
+        self.validation_target = self.file_read('split_dataset_validation_target.csv')
 
     def data_summary(self):
         print 'Data shape: ', self.dataset.shape
         print self.dataset.describe()
 
-    def learn_naive_bayes(self):
-        nb = MultinomialNB()
-        self.clf = nb.fit(input, output)
-        #print self.clf
+    def learn_svm(self):
+        print 'Learning SVM...'
+        self.clf_svm = svm.SVC()
+        self.clf_svm.fit(self.train_input, self.train_target['OutcomeType'])
+        print 'Done.'
 
-    def predict_naive_bayes(self):
-        print 'Predicting rows %d through %d...' % (self.validation_start_index, self.validation_end_index)
-        y_pred = self.clf.predict(self.input_validation, self.output_validation)
-        total = input_validation.shape[0]
-        correct = (output_validation == y_pred).sum()
+    def predict_svm(self):
+        print 'Predicting SVM...'
+        #print 'Predicting rows %d through %d...' % (self.validation_start_index, self.validation_end_index)
+        y_pred = self.clf_svm.predict(self.validation_input)
+        y_pred = y_pred.reshape(y_pred.shape[0], 1)
+        total = self.validation_input.shape[0]
+        print  y_pred.shape
+        print self.validation_target.shape
+        correct = (self.validation_target == y_pred).sum()
         accuracy = (float(correct)/float(total))*100.0
         print "Number of mislabeled points out of a total %d points: %d" % (total, total-correct)
         print "Accuracy: %.1f%%" % (accuracy)
+        print 'Done.'
         
     def missing_values(self):
         missmap.missmap(self.dataset)
@@ -119,34 +166,62 @@ class animal_shelter:
         print 'scikit-learn version: ', sklearn.__version__
 
     def save_to_file(self, df, filename):
+        if self.flag_save_files:
+            filename = '../output/' + filename
+            print 'Saving to file %s...' % (filename)
+            df = pandas.DataFrame(df)
+            df.to_csv(filename, index=False)
+            print 'Done.'
+
+    def file_read(self, filename):
         filename = '../output/' + filename
-        print 'Saving to file %s...' % (filename)
-        df = pandas.DataFrame(df)
-        df.to_csv(filename, index=False)
+        print 'Reading file %s...' % (filename)
+        df = pandas.read_csv(filename)
         print 'Done.'
+        return df
 
 # Init script
 app = animal_shelter()
 
-# Step 1: Load train dataset (read CSV)
-app.load_train_dataset()
+if not app.flag_restore_session:
 
-# Step 2: Parse age
-app.convert_age_to_numerical()
+    # Step 1: Load train dataset (read CSV)
+    app.load_train_dataset()
 
-# Step 3: Shuffle?
+    # Step: Shuffle?
+    app.shuffle()
 
-# Step 4: Select attributes
-# Remover AnimalID e OutcomeSubType
-app.select_attributes()
+    # Keep most popular breeds
+    app.keep_most_popular_values_for_feature('Breed', 5)
 
-# Step 5: Convert categorical variables to numeric
-#app.categorical_to_numerical_one_hot()
-app.categorical_to_numerical_unique()
+    # Keep most popular names
+    app.keep_most_popular_values_for_feature('Name', 5)
 
-# Step 6: Replace missing values
-# Média? Moda?
-app.fix_missing()
+    # Keep most popular colors
+    app.keep_most_popular_values_for_feature('Color', 5)
+
+    # Step 2: Parse age and normalize from 0 to 1, NA's as 0.5
+    app.convert_age_to_numerical()
+
+    # Step: Replace missing values
+    # Média? Moda?
+    app.fix_missing()
+
+    # Step 4: Select attributes
+    # Remover AnimalID e OutcomeSubType
+    app.select_attributes()
+
+    # Step: Convert target variable to numerical
+    app.target_to_numerical()
+
+    # Step 5: Convert categorical variables to numeric
+    #app.categorical_to_numerical_one_hot()
+    app.categorical_to_numerical_unique()
+
+    app.split_dataset(ts=0, te=1000, vs=2000,ve=3000)
+
+else:
+    app.restore_session()
 
 # Step 7: Validation
 #    - X-Validation
@@ -157,3 +232,9 @@ app.fix_missing()
 #            - Apply Model
 
 # Step 8: Validate performance (accuracy, etc)
+
+# Quick splitting dataset in train and validation subsets
+
+# Step: Learn Naive Bayes
+app.learn_svm()
+app.predict_svm()
